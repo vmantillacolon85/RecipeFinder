f4b4490a7281ceea7be4f0c7f9c4266b05d44bdf
Avery Sarichny <averysarichny@cpe-67-240-191-189.rochester.res.rr.com>
2021-01-16 18:05:18 -0500

    debugging

--- server.js
+++ server.js
@@ -2,22 +2,23 @@
 //  DEPENDENCIES
 //==================
 
-const express = require("express")
-const mongoose = require('mongoose')
-const app = express()
-require("dotenv").config()
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 //================
 //  MIDDLEWARE
 //================
-app.use(express.json())
-app.use(express.static("public"))
+app.use(express.json());
+app.use(express.static("public"));
 
-const recipesController = require('./controllers/recipes_controller.js')
-app.use('/recipes', recipesController)
+const recipesController = require("./controllers/recipes_controller.js");
+app.use("/recipes", recipesController);
 
+//testing testing
 
 //===================
 //  ERROR & SUCCESS
@@ -26,19 +27,22 @@ mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-})
+});
 
-mongoose.connection.on('error', (err) =>
+mongoose.connection.on("error", (err) =>
   console.log(
     err.message,
-    'is mongod not running/Problem with atlas connection?'
+    "is mongod not running/Problem with atlas connection?"
   )
-)
-mongoose.connection.on('connected', () =>
-  console.log('mongo connected: ', MONGODB_URI)
-)
-mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+);
+mongoose.connection.on("connected", () =>
+  console.log("mongo connected: ", MONGODB_URI)
+);
+mongoose.connection.on("disconnected", () => console.log("mongo disconnected"));
 
 app.listen(PORT, () => {
-console.log( ':cherries::lemon:Listening on port:kiwifruit::watermelon::', PORT)
+  console.log(
+    ":cherries::lemon:Listening on port:kiwifruit::watermelon::",
+    PORT
+  );
 });
